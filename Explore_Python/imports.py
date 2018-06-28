19659c7b31815f14167a9e0a0b82cc3c120e6622
Variegatedq <f2013225@dubai.bits-pilani.ac.in>
2018-06-28 16:28:06 -0400

    J28
    Updates on 28th June : Scripts updated, connections established

---
+++ Explore_Python/imports.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import numpy as np
+import io
+from io import StringIO, BytesIO
+import scipy
+import pandas as pd
+import skimage
+import json
+import SciScript_Python
+from SciScript_Python import py3
+from SciScript_Python.py3 import SciServer
+from SciScript_Python.py3.SciServer import SkyServer, Authentication, CasJobs, SkyQuery
+    
+print(x)
