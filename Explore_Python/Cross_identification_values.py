469da97fe49791a07df020cf0098221dbfdd2e03
Variegatedq <f2013225@dubai.bits-pilani.ac.in>
2018-07-03 17:23:38 -0400

    July3

--- Explore_Python/Cross_identification_values.py
+++ Explore_Python/Cross_identification_values.py
@@ -1,59 +1,61 @@
+#!/usr/bin/env python import *
 
-import os
-import sys
 from __main__ import *
-
+from imports import *  
+from Image_Cut_Out import *
 
 #CASJOBS#
 
     ### getting all values under cross identification with OBJID###
 
-tabel=pd.DataFrame[index=[1,2,3,4,5]]  
-    
+tabel=pd.DataFrame() 
        
-i=1        
+i=0       
 y=ob_id
+yra=ra
+ydec=dec
 q=None
 
          #values from table USNO#
 
-tabel.loc[i]=['proper_motion', 'mura_err', 'mudec_err', 'angle']
-i+=1
+#tabel.loc[i]=['proper_motion', 'mura_err', 'mudec_err', 'angle']
+#i+=1
 
-tabel.loc[i]=[SciServer.CasJobs.executeQuery(sql="select propermotion, muraerr, mudecerr, angle from USNO n where n.OBJID=y", context='DR14', format='pandas')]
+a=SciServer.CasJobs.executeQuery(sql="select n.propermotion, n.muraerr, n.mudecerr, n.angle from USNO n where n.OBJID=y", context='DR14', format='pandas')
+a=np.transpose(a)
+tabel.loc[i]=[a]
 i+=i        
         
         #values from tables WISE_xmatch and WISE_allsky#
 
-tabel.loc[i]=['wise_cntr','w1mag', 'w2mag', 'w3mag', 'w4mag']        
-i+=i 
+#tabel.loc[i]=['wise_cntr','w1mag', 'w2mag', 'w3mag', 'w4mag']        
+#i+=i 
 
-tabel.loc[i+1]=[SciServer.CasJobs.executeQuery(sql="select wise_cntr,w1mag, w2mag, w3mag, w4mag, from WISE_xmatch h and WISE_skyall t where h.OBJID=y and t.OBJID=y",context='DR14',format='pandas')]
+a=SciServer.CasJobs.executeQuery(sql="select h.wise_cntr, t.w1mag, t.w2mag, t.w3mag, t.w4mag, from WISE_xmatch h and WISE_skyall t where h.sdss_objid=y and t.ra=yra and t.dec=ydec",context='DR14',format='pandas')
+a=np.transpose(a)
+tabel.loc[i]=[a]
 i+=i       
         
-
     #values from ROSAT and RC3#
     
-tabel.loc[i]=['cps', 'hr1', 'hr2', 'ext', hr1, hr2, ext]
-i+=1
+#tabel.loc[i]=['cps', 'hr1', 'hr2', 'ext', hr1, hr2, ext]
+#i+=1
 
-q= SciServer.CasJobs.executeQuery(sql="select cps, hr1, hr2, ext,cps, hr1, hr2, ext from RC3 c and ROSAT q where c.OBJID= q.OBJID= y", context='DR14', format='pandas')
+q= SciServer.CasJobs.executeQuery(sql="select q.CPS, q.HR1,q.HR2,q.EXT,q.CAT,  from RC3 c and ROSAT q where c.objID= q.OBJID= y", context='DR14', format='pandas')
 
 if (q == NONE):
     print("There is no ROSAT, RC3 data for this object")
 else:
-    tabel.loc[i]=[q]
+    tabel.loc[i]=[np.transpose(q)]
     i+=1
       
         
     #values from tables FIRST and TwoMASS #
 
-tabel.loc[i]= ['j','h','k', 'phQual']    
-i+=1    
+#tabel.loc[i]= ['j','h','k', 'phQual']    
+#i+=1    
 
-tabel.loc[i] = [SciServer.CasJobs.executeQuery(sql="select j,h,k, phQual from TwoMASS s and First f where s.OBJID= f.OBJID= y", context='DR14', format='pandas')]
+a=SciServer.CasJobs.executeQuery(sql="select j,h,k, phQual from TwoMASS s and First f where s.OBJID= f.OBJID= y", context='DR14', format='pandas')
+tabel.loc[i]=[np.transpose(a)]
 i+=1
     
-
-
-print(tabel)
--- Explore_Python/Imaging_values.py
+++ Explore_Python/Imaging_values.py
@@ -1,67 +1,70 @@
 
-#!/usr/bin/env python
+#!/usr/bin/env python import *
 
 import os
 import sys
 
+from Image_Cut_Out import ob_id
 from __main__ import *
 
 
 
-x = ob_id
+x = int(ob_id)
 token=Authentication.getToken()
-          
-
+I=0          
+Answer=pd.DataFrame()
 
 print("Imaging ")
 
   
-
+sql_query=('select * from PhotoTag g where g.objID='+x) 
              ### getting values from table PhotoTag  ###
 
-      
+Phtag=SkyServer.sqlSearch(sql=sql_query, dataRelease='DR14')     
+Phtag= np.transpose(Phtag)       
+### ***************** Function PhotoTag on the LEFT sidebar *****************###    
+            
+        ### getting values from table photoobjall ###
         
-    
-             ### getting values from table photoobjall ###
-          
-        
-sql_query="select p.clean, p.type, p.u, p.g, p.r , p.I, p.z, p.err_u, p.err_g, p.err_r, p.err_i, p.err_z from PhotoObjAll p where p.objID=x"
-Answer.loc[I]=['clean', 'type', 'u', 'g', 'r' ,'I','z,err_u', 'err_g,err_r','err_i,err_z']
+sql_query="select p.clean, p.type, p.u, p.g, p.r , p.I, p.z, p.err_u, p.err_g, p.err_r, p.err_i, p.err_z from PhotoObjAll p where p.objID= "+x)
+#Answer.loc[I]=['clean', 'type', 'u', 'g', 'r' ,'I','z,err_u', 'err_g,err_r','err_i,err_z']
+#I+=1
+a=SkyServer.sqlSearch(sql=sql_query, dataRelease="DR14")
+a=np.transpose(a)
+Answer.loc[I]=[a]
 I+=1
-Answer.loc[I]=[SkyServer.sqlSearch(sql=sql_query, dataRelease="DR14")]
+       
+              ### getting values from table PhotoObj and Photoz ###
+              
+#Answer.loc[I]=['mode', 'mdj','nDetect1', 'parentID', 'nChild', 'extinction_r', 'petroRad', 'petroRadErr_r' 
+#I+=1
+a=SkyServer.sqlSearch(sql="select b.mode, b.mdj, b.nDetect-1, b.parentID, b.nChild, b.extinction_r, b.petroRad, b.petroRadErr_r, from PhotoObj b where b.objID=x", dataRelease="DR14")
+a=np.transpose(a)
+Answer.loc[I]=[a]
 I+=1
-        
-    
-             ### getting values from table PhotoObj and Photoz ###
-        
-        
-Answer.loc[I]=['mode', 'mdj','nDetect-1','parentID','nChild','extinction_r','petroRad','petroRadErr_r']
-I+=1
-Answer.loc[I]=[SkyServer.sqlSearch(sql="select b.mode, b.mdj, b.nDetect-1, b.parentID, b.nChild, b.extinction_r, b.petroRad, b.petroRadErr_r, from PhotoObj b where b.objID=x", dataRelease="DR14")]
-I+=1
-Answer.loc[I]= ['z','zerr']
-I+=1
-Answer.loc[I]=[SkyServer.sqlSearch(sql="select h.z,h.zerr from Photoz h where h.objID=x", dataRelease="DR14")]         
+#Answer.loc[I]= ['z','zerr']
+#I+=1
+a=SkyServer.sqlSearch(sql="select h.z,h.zerr from Photoz h where h.objID=x", dataRelease="DR14")
+a=np.transpose(a)
+Answer.loc[I]=[a]
 I+=1       
-        
-    
+            
         ### getting values from table zooSpec on shape of object  ###
-Answer.loc[I]=['Morphology']
-I+=1
+#Answer.loc[I]=['Morphology']
+#I+=1
 
 s=(SkyServer.sqlSearch(sql="select z.spiral from zooSpec z where z.objID=x", dataRelease="DR14"))
 e=(SkyServer.sqlSearch(sql="select z.elliptical from zooSpec z where z.objID=x", dataRelease="DR14"))
 u=(SkyServer.sqlSearch(sql="select z.uncertain from zooSpec z where z.objID=x", dataRelease="DR14"))
-if (s):
-    Answer.loc[I]=['Spiral']
+if ((s==1) or (s>e)):
+    Answer.loc[I]=['Morphology','Spiral']
     I+=1
-elif (e) :
-    Answer.loc[I]=['Elliptical']
+elif ((e==1) or (e>s)) :
+    Answer.loc[I]=['Morphology','Elliptical']
     I+=1
 else:
-    Answer.loc[I]=['Uncertain']
+    Answer.loc[I]=['Morphology','Uncertain']
     I+=1
-    
     
 
    
