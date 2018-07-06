20266c606407058b2794509d96266b303d9bc696
Variegatedq <f2013225@dubai.bits-pilani.ac.in>
2018-07-06 18:58:42 -0400

    changed to functions

--- Explore_Python/Imaging_values.py
+++ Explore_Python/Imaging_values.py
@@ -2,86 +2,73 @@
 
 import os
 import sys
-
 from Image_Cut_Out import ob_id
 from __main__ import *
-
-x=int(ob_id)
 token=Authentication.getToken()
-I=0; i=0          
-Answer=pd.DataFrame(index=[0], columns=['N','V'])
-print(Answer)
-print("Imaging ")
 
-sql_query=("select * from PhotoTag g where g.objID=" + str(x))
-             ### getting values from table PhotoTag  ###
-
-Phtag=SkyServer.sqlSearch(sql=sql_query, dataRelease='DR14')     
-Phtag=np.transpose(Phtag)
-#print(Phtag)
-### ***************** Function PhotoTag on the LEFT sidebar ***************** ###    
-            
-        ### getting values from table photoobjall ###
+def display_image():
+    
+    print("Imaging ")
+    x=int(ob_id)
+    I=0; i=0;          
+    Answer=pd.DataFrame(index=[0], columns=['N','V'])
         
-sql_query=("select p.clean, p.type, p.u, p.g, p.r , p.I, p.z, p.err_u, p.err_g, p.err_r, p.err_i, p.err_z from PhotoObjAll p where p.objID= " + str(x))
-#Answer.loc[I]=['clean', 'type', 'u', 'g', 'r' ,'I','z,err_u', 'err_g,err_r','err_i,err_z']
-#I+=1
-a=(np.transpose(SkyServer.sqlSearch(sql=sql_query, dataRelease="DR14")))
-print(a)
-#print(len(a.index))
-for index,row in a.iterrows():
-    Answer.loc[I]=((row.name,row[0]))
-    I+=1
-Answer.loc[I]=('*',0)
-I+=1
+    sql_query=("select p.clean, p.type, p.u, p.g, p.r , p.I, p.z, p.err_u, p.err_g, p.err_r, p.err_i, p.err_z from PhotoObjAll p where p.objID= " + str(x))
+    a=(np.transpose(SkyServer.sqlSearch(sql=sql_query, dataRelease="DR14")))
+    if a.empty:
+        print("There are no u, g, r, I, z values for this object")
+    else:
+        for index,row in a.iterrows():
+            Answer.loc[I]=((row.name,row[0]))
+            I+=1
+        Answer.loc[I]=('*','*')
+        I+=1
     
-print(Answer)
-#Answer.loc[I]=[a]
-#I+=1
-       
-              ### getting values from table PhotoObj and Photoz ###
-              
-#Answer.loc[I]=['mode', 'mdj','nDetect1', 'parentID', 'nChild', 'extinction_r', 'petroRad', 'petroRadErr_r' 
-#I+=1
-sql_query=("select b.mode, b.mdj, b.nDetect-1, b.parentID, b.nChild, b.extinction_r, b.petroRad, b.petroRadErr_r from PhotoObj b where b.objID="+ str(x))
-a=(np.transpose(SkyServer.sqlSearch(sql=sql_query, dataRelease="DR14")))
-print(a)
-
-for index,row in a.iterrows():
-    Answer.loc[I]=((row.name,row[0]))
-    I+=1
-Answer.loc[I]=('*',0)
-I+=1
-print(Answer)
-
-a=(np.transpose(SkyServer.sqlSearch(sql="select h.z,h.zerr from Photoz h where h.objID=" + str(x), dataRelease="DR14")))
-for index,row in a.iterrows():
-    Answer.loc[I] = ((row.name,row[0]))
-    I+=1
-Answer.loc[I]=('*',0)
-I+=1
-
-            
-        ### getting values from table zooSpec on shape of object  ###
-#Answer.loc[I]=['Morphology']
-#I+=1
-
-s=(SkyServer.sqlSearch(sql="select z.spiral from zooSpec z where z.objID=x", dataRelease="DR14"))
-e=(SkyServer.sqlSearch(sql="select z.elliptical from zooSpec z where z.objID=x", dataRelease="DR14"))
-u=(SkyServer.sqlSearch(sql="select z.uncertain from zooSpec z where z.objID=x", dataRelease="DR14"))
-if ((s==1) or (s>e)):
-    Answer.loc[I]=['Morphology','Spiral']
-    I+=1
-elif ((e==1) or (e>s)) :
-    Answer.loc[I]=['Morphology','Elliptical']
-    I+=1
-else:
-    Answer.loc[I]=['Morphology','Uncertain']
-    I+=1
+    sql_query=("select z.spiral from zooSpec z where z.objid="+ str(x))
+    a=(SkyServer.sqlSearch(sql=sql_query, dataRelease="DR14"))
+    if a.empty:
+        s=0
+    else:
+        s=a.index[0]
+    sql_query=("select z.elliptical from zooSpec z where z.objid="+ str(x))
+    a=(SkyServer.sqlSearch(sql=sql_query, dataRelease="DR14"))
+    if a.empty:
+        e=0
+    else:
+        e=a.index[0]
+    if ((s==1) or (s>e)):
+        Answer.loc[I]=('Morphology','Spiral')
+        I+=1
+    elif ((e==1) or (e>s)) :
+        Answer.loc[I]=('Morphology','Elliptical')
+        I+=1
+    else:
+        Answer.loc[I]=('Morphology','Uncertain')
+        I+=1
+    return Answer
+                   
+def link_phobj():
     
+    sql_query=("select b.mode, b.mdj, b.nDetect-1, b.parentID, b.nChild, b.extinction_r, b.petroRad, b.petroRadErr_r from PhotoObj b where b.objID="+ str(x))
+    Phobj=(np.transpose(SkyServer.sqlSearch(sql=sql_query, dataRelease="DR14")))
+    if Phobj.empty:
+        print("There are no Photo Object values for this object")
+    else:
+        return Phobj
 
-   
-    
-    
+def link_phtag():
 
+    sql_query=("select * from PhotoTag g where g.objID=" + str(x))
+    Phtag=(np.transpose(SkyServer.sqlSearch(sql=sql_query, dataRelease='DR14')))     
+    if Phtag.empty:
+        print("There are no tags for this object")
+    else:
+        return Phtag
 
+def link_phz():
+    sql_query=("select h.z,h.zerr from Photoz h where h.objID ="+ str(x))
+    Phz=(np.transpose(SkyServer.sqlSearch(sql=sql_query, dataRelease="DR14")))
+    if Phz.empty:
+        print("There are no Photoz values for this object")
+    else:
+        return Phz
---
+++ Explore_Python/fn_imports.py
@@ -0,0 +1,6 @@
+import Imaging_values
+from Imaging_values import *
+import Spectra_values
+from Spectra_values import *
+import Cross_identification_values
+from Cross_identification_values import *
