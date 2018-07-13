bc831bdfe1ae508a68499d8f55798fd8530499ef
Variegatedq <f2013225@dubai.bits-pilani.ac.in>
2018-07-12 20:08:17 -0400

    exceptions and formatting

--- Explore_Python/fn_imports.py
+++ Explore_Python/fn_imports.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python import *
 
-import Imaging_values
-from Imaging_values import *
-import Spectra_values
-from Spectra_values import *
-import Cross_identification_values
-from Cross_identification_values import *
+import imaging_values
+from imaging_values import *
+import spectra_values
+from spectra_values import *
+import cross_id_values
+from cross_id_values import *
 import apooge_values
 from apooge_values import *
 import manga_values
--- Explore_Python/manga_values.py
+++ Explore_Python/manga_values.py
@@ -1,36 +1,50 @@
 # coding: utf-8
 #!/usr/bin/env python
-'''MaNGA values
-~~~~~~~~~~~~~~~~~~~~~~~~~~~
 '''
-from Image_Cut_Out import *
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
+IFU spectrum values
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:Spectrum values observed using IFU(Integral Field Units) for the celestial body in question.
+
+:param:: 'a' - temporary data frame that consolidates o/p to a single format  
+:param:: I(capital i["eye"]) is the counter; indicating the next row of the data frame. 
+'''
+
+from img_cut import *
 from __main__ import *
 
+token=Authentication.getToken()
+#data_release="DR14"
+
 def display_manga():
-    """
+    '''
+
+    :Display:: Primary values for the MaNGA portion of the query.
+    :param:: No input parameters
+    :Return:: A pandas' data frame, 'manga' 
+    :Raise:: Warning for missing or corrupted output
     
-    :Display the main values for the Imaging portion of the query.
-    :Return:: Pandas data frame with organized values to be printed.
-    :Raises an exception if the query is invalid.
-    
-    ..seealso::
-    """
+    ..seealso:: manga_values.__doc__
+    '''
     
     print("MaNGA") 
-    m=manga_id
-    
-    sql_query=("select h.ifura, h.ifudec, h.mangaid, h.mngtarg1, h.mngtarg2, h.mngtarg3, h.objdec, h.objra, h.plateifu, h.mjdmax, h.redsn2. h.drp3qual, h.bluesn2 from manDrpAll h where h.mangaid =" +str(m))
-    a=SciServer.CasJobs.executeQuery(sql=sql_query, context="DR14", format='pandas')
-    a=np.transpose(a)
-    if a.empty:
-        print("There are no MaNGA values for this celestial object")
-        return 0
+    I=0
+    manga=pd.dataFrame(index=[0], columns=['N','V'])
+    try:
+        sql_query=("select h.ifura, h.ifudec, h.mangaid, h.mngtarg1, h.mngtarg2, h.mngtarg3,h.objdec"+
+                   " h.objra, h.plateifu, h.mjdmax, h.redsn2. h.drp3qual, h.bluesn2 from manDrpAll h where h.mangaid=" + str(manga_id))
+        a=SciServer.CasJobs.executeQuery(sql=sql_query, context=data_release, format='pandas')
+        a=np.transpose(a)
+        if a.empty:
+            print("There are no MaNGA values for this celestial object")
+            pass
+        else:
+            manga.loc[I]=('MaNGA Observation(s)','*'); I+=1;
+            for index,row in a.iterrows():
+                man.loc[I]=((row.name,row[0]))
+                I+=1
+    except:
+        print("Unexpected error: "+ sys.exc_info()[0])
     else:
-        man.loc[I]=('MaNGA Observation(s)','*'); I+=1;
-        for index,row in a.iterrows():
-            man.loc[I]=((row.name,row[0]))
-            I+=1
-            return man
-         
-    
-    
+        return manga
