e4af986c51e6209c210402700d52f0f57d377b4d
Variegatedq <f2013225@dubai.bits-pilani.ac.in>
2018-07-25 17:03:00 -0400

    minor update 3 July25

--- ExplorePython/imports.py
+++ ExplorePython/imports.py
@@ -1,8 +1,18 @@
 # coding: utf-8
 #!/usr/bin/env python
 
-import sys
-import os
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+
+from setuptools import setup, find_packages
+setup(
+    name = "SciServer",
+    version = "1.10.2",
+    packages = find_packages(),
+)
+
 import numpy as np
 import io
 from io import StringIO, BytesIO
@@ -14,10 +24,6 @@ import matplotlib.img as imgm
 import json
 import warnings
 import re
-import inspect
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0,parentdir) 
 # import SciScript_Python
 # from SciScript_Python import py3
 # from SciScript_Python.py3 import SciServer
