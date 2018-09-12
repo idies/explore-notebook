b67c90be02142c9f837f56e3301b4a6de83a0a2a
Variegatedq <jmalani@github.com>
2018-09-12 16:27:07 -0400

    minor update - Working version

--- ExplorePython/missing_values.py
+++ ExplorePython/missing_values.py
@@ -1,15 +1,14 @@
 #coding: utf-8
 #!/usr/bin/env python import *
-
-from fn_imports import *
-from imports import *
 import getpass
+from imports import *
+
 '''
 python file containing miscellaneous functions
 this file can be accessed by all other display functions
 '''
-    
-def get_radec(objid):
+
+def get_radec_obj(objid):
     
     
     x=pd.DataFrame(index=[0], columns=["N","V"])
@@ -17,64 +16,89 @@ def get_radec(objid):
     x["V"]=pd.Series([], dtype=object)
     sql_query="select * from FIRST where objID= " +str(objid)
     x=CasJobs.executeQuery(sql=sql_query, context=data_release, format="pandas") 
-    ra=x[0]['ra']
-    dec=[0]['dec']
-    return(str(ra)+str(dec))
+    if (x.empty):
+        print("There are currently no objects with this object ID. Please verify your input an try again")
+        raise ValueError
+        
+    ra=format(x[0]['ra'],'f')
+    dec=format([0]['dec'],'f')
+    return((ra),(dec))
     
+def get_radec_spec(specid):
+    x=pd.DataFrame(index=[0], columns=["N","V"])
+    x["N"]=pd.Series([], dtype=str)
+    x["V"]=pd.Series([], dtype=object)
+    sql_query='Select ra,dec from SpecObjAll where specObjID='+str(specid)
+    x=CasJobs.executeQuery(sql=sql_query, context=data_release, format="pandas") 
+    if(x.empty):
+        print("There are currently no objects with this optical spetcral ID. Please verify your input an try again")
+        raise ValueError
+    ra=format(x[0]['ra'],'f')
+    dec=format([0]['dec'],'ra')
+    return((ra),(dec))
+
+
 def get_objid(ra=197.614455635, dec=18.438168849):
     '''
     Returns:: big int object_id from given ra and dec of the object
-    input:: ra(right ascension), dec(declination)
+    input(optional):: ra(right ascension), dec(declination)
     param:: 'x' is a pandas data frame used to store query values
     '''
-    if(ra==0 or dec==0):
-        print("Missing: function arguments(ra,dec)")
-        return 0
-    if (ra is 197.614455635 and dec is 18.438168849):
-        print("Invalid input argument. Display: Default object specs displayed")
-    x=pd.DataFrame(index=[0], columns=["N","V"])
-    x["N"]=pd.Series([], dtype=str)
-    x["V"]=pd.Series([], dtype=object)
+    try:
+        
+        if(ra is 197.614455635 and dec is 18.438168849): 
+            print("default specs displayed")
+        else: 
+            pass
+        
+        x=pd.DataFrame(index=[0], columns=["N","V"])
+        x["N"]=pd.Series([], dtype=str)
+        x["V"]=pd.Series([], dtype=object)
 
-    sql_query='select * from fGetNearestObjEq(' + str(ra) + ',' + str(dec)+ ', 0.2)'
-    x=CasJobs.executeQuery(sql=sql_query, context=data_release, format='pandas')
-   
-    t=float(x["objID"])
-    t1=f'{t:.0f}'
-    objid=float(t1)
-    view(x)
-    if (bool(objid) is False):
-        print("No object id found. Please verify argument parameters and try again")
-        return 0
-    else:
-        return objid
+        sql_query="select * from fGetNearestObjIdEq(" +str(ra)+ "," +str(dec)+ ",0.2)"
+        x=CasJobs.executeQuery(sql=sql_query, context=data_release, format="pandas")
+        objid=format(x.iloc[0]['objID'],'f')
+        if (bool(objid) is False):
+            raise ValueError("No object id found. Please verify argument parameters and try again")
+            pass
+        else:
+            return objid
 
-def get_specid(ra=197.614455635, dec=18.438168849):
+    except (NameError,ValueError,TimeoutError) as e:
+        print(str(e)+ "occured. Please verify your values and try again")
+
+def get_specid(ra=197.614455635 , dec=18.438168849):
     '''
     Derives spectral id when just ra and dec are available
     Returns:: big int specid storing the IR and optical spectrum values for the chosen object
-    input:: ra(right ascension), dec(declination) of the celestial body in question; both initialized to 0
+    input(optional):: ra(right ascension), dec(declination) of the celestial body in question; both initialized to 0
     param:: 'x' is a pandas data frame used to store query values
     '''
-    if (ra==0 or dec==0):
-        print("Missing: function arguments (ra,dec)")
-        return 0
-    if (ra is 197.614455635 and dec is 18.438168849):
-        print("Invalid input argument. Display: Default object specs displayed")
-    x=pd.DataFrame(index=[0], columns=["N","V"])
-    x["N"]=pd.Series([], dtype=str)
-    x["V"]=pd.Series([], dtype=object)
+    try:
 
-    sql_query="select * from fGetNearestSpecObjEq(" + str(ra) + "," + str(dec)+  ", 0.2)"
-    x=CasJobs.executeQuery(sql=sql_query, context=data_release, format='pandas')
-    t=float(x['specObjID'])
-    t1=f'{t:.0f}'
-    specid=float(t1)
-    if (bool(specid) is False):
-        print("No spectrum id found. Please verify argument parameters and try again")
-        return 0
-    else:
-        return specid
+       
+        if (ra is 0 and dec is 0):
+            print("Missing: function arguments(ra,dec)")
+            return 0
+        elif (ra is 197.614455635 and dec is 18.438168849):
+            print("Invalid input argument. Display: Default object specs displayed")
+            return ((2947691243863304192.0))
+        else:
+            x=pd.DataFrame(index=[0], columns=["N","V"])
+            x["N"]=pd.Series([], dtype=str)
+            x["V"]=pd.Series([], dtype=object)
+
+            sql_query="select * from fGetNearestSpecObjIdAllEq(" + str(ra) + "," + str(dec)+  ", 0.2)"
+            x=CasJobs.executeQuery(sql=sql_query, context=data_release, format='pandas')
+            specid=format(x.iloc[0]['specObjID'],'f')
+#             specid=x.iloc[0]['specObjID'] 
+        if (bool(specid) is False):
+            raise ValueError("No spectrum id found. Please verify argument parameters and try again")
+            pass
+        else:
+            return specid
+    except (NameError,ValueError,TimeoutError) as e:
+        print(str(e)+ "has occurred. Please verify your values and try again")
 
 def Auth():
     '''
@@ -87,34 +111,43 @@ def Auth():
     '''
 ## Authentication token for SciServer ##
     try:
-        
         auth_username=getpass.getpass('User Name: ')
         auth_pass=getpass.getpass('Password: ')
-        
         token=Authentication.login(auth_username,auth_pass)
     except (ValueError, TypeError) as e:
         print(str(e) + "Please verify your username and password and try again")
         Auth()
     else:
         token=Authentication.getToken()
-        return 0
+        return 2
     
 
-def display_obj_image(ra=197.614455635, dec=18.438168849):
-    if (ra is 197.614455635 and dec is 18.438168849):
-        print("Invalid input argument. Display: Default object specs")    
-    ra1=ra; dec1=dec
-    pixel_scale=0.2
-    img = SkyServer.getJpegImgCutout(ra=ra1, dec=dec1, scale = pixel_scale) 
-    #plt.savefig(plt.imshow(img), bbox_inches='tight')
-    #img, (a, b) = plt.subplots()
-    fig=plt.imshow(img, interpolation = 'nearest')
-    fig.axes.get_xaxis().set_visible(False)
-    fig.axes.get_yaxis().set_visible(False)
-    #fig.set_axisbelow(True)
-    #plt.yaxis.grid(linestyle='dashed')
-    #plt.grid(b=True, which='major', axis='both', ls='-')
-    plt.show()
+def display_obj_image(ra=197.614455635 , dec=18.438168849):
+    try:
+        
+        if(ra is 0 and dec is 0):
+            print("Missing: function arguments(ra,dec)")
+            return 0
+        elif (ra is 197.614455635 and dec is 18.438168849):
+            print("Invalid input argument. Display: Default object specs")    
+            pass
+        else:
+            pass
+        
+        ra1=ra; dec1=dec;
+        pixel_scale=0.2
+        img = SkyServer.getJpegImgCutout(ra=ra1, dec=dec1, scale = pixel_scale) 
+        #plt.savefig(plt.imshow(img), bbox_inches='tight')
+        #img, (a, b) = plt.subplots()
+        fig=plt.imshow(img, interpolation = 'nearest')
+        fig.axes.get_xaxis().set_visible(False)
+        fig.axes.get_yaxis().set_visible(False)
+        #fig.set_axisbelow(True)
+        #plt.yaxis.grid(linestyle='dashed')
+        #plt.grid(b=True, which='major', axis='both', ls='-')
+        plt.show()
+    except:
+        print("Unexpected Error. Pleae try again")
     
 def view(df):
     css = """<style>
@@ -130,13 +163,4 @@ def view(df):
     s += '</script>'
 
     return(HTML(s+css))
-
-try:
-    token=Authentication.getToken()
-    if token is None:
-        Auth()
-except (NameError, TypeError) as e:
-    print(str(e)+" Please verify your credentials and try again")
-    Auth()
-else:
-    token = Authentication.getToken()
+        
